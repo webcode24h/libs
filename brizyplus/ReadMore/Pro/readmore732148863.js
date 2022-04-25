1554d409c82752e220f1cf8372102406107936df
webcode24h <90999027+webcode24h@users.noreply.github.com>
2022-04-26 00:17:36 +0700

    Update readmore732148863.js

--- brizyplus/ReadMore/Pro/readmore732148863.js
+++ brizyplus/ReadMore/Pro/readmore732148863.js
@@ -1,23 +1,12 @@
 const readmore_all = document.querySelectorAll('.readmore');
-
 for (const readmore of readmore_all) {
-
-  if (readmore.clientHeight > 250) {
-
-  readmore.classList.add('open_readmore');
-
+	if (readmore.clientHeight > 250) {
+		readmore.classList.add('open_readmore');
+	}
 }
-
-}
-
 var open_readmore_all = document.querySelectorAll('.open_readmore');
-
 Array.from(open_readmore_all).forEach(open_readmore => {
-
-  open_readmore.addEventListener( 'click', function() {
-
-    open_readmore.classList.toggle('open_readmore');
-
-  });
-
+	open_readmore.addEventListener('click', function() {
+		open_readmore.classList.toggle('open_readmore');
+	});
 });
